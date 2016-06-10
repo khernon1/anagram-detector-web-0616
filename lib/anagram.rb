397cbcc25e51dcc803da194e0fa876fb28b6de0d
kevin hernon <kevinhernon@kevins-MacBook-Air.local>
2016-06-09 20:38:17 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+#given a word and an array of words, return everything that has the same
+#letters of the word = the reverse of each word in the array
+
+#need to iterate through the array in the match and add it to a new array if match
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word.split("")
+  end
+
+  def match(arr)
+      arr.select do |word|
+        word = word.split("")
+        word.sort == @word.sort
+      end
+  end
+
+end
